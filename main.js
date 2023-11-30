e732158d292f956d17e0a892507bbd8fd920f5b2
GlenKurio <gleb.kurip@gmail.com>
2023-11-29 18:41:44 -0600

    basic functionality

--- main.js
+++ main.js
@@ -7,20 +7,43 @@ import {
   doc,
   serverTimestamp,
   collection,
+  deleteDoc,
 } from "firebase/firestore";
 
+/////////////////////////
 // Selectors
+/////////////////////////
 const addButtonEl = document.getElementById("addButton");
 const inputFieldEl = document.getElementById("input");
 const shoppingList = document.getElementById("shoppingList");
 
+/////////////////////////
 // Global Consts
+/////////////////////////
 const shoppingListCollection = "shoppingList";
 const shoppingListRef = collection(db, shoppingListCollection);
 
 //////////////////////
 // Firebase Calls
 /////////////////////
+document.addEventListener("DOMContentLoaded", fetchListFromDB(shoppingListRef));
+
+function fetchListFromDB(query, user) {
+  onSnapshot(query, (listSnapshot) => {
+    clearList(shoppingList);
+
+    if (listSnapshot.size === 0) {
+      return showEmptyListElement();
+    }
+    listSnapshot.forEach((doc) => {
+      const { item } = doc.data();
+      const itemId = doc.id;
+
+      createListItem(item, itemId);
+    });
+  });
+}
+
 async function addItemToDB(itemBody) {
   try {
     const listItemRef = await addDoc(collection(db, shoppingListCollection), {
@@ -33,28 +56,29 @@ async function addItemToDB(itemBody) {
   }
 }
 
-function fetchListFromDB(query, user) {
-  onSnapshot(query, (listSnapshot) => {
-    clearList(shoppingList);
-    listSnapshot.forEach((doc) => {
-      const { item } = doc.data();
-
-      createListItem(item);
-    });
-  });
+async function deleteItemFromList(docId) {
+  await deleteDoc(doc(db, shoppingListCollection, docId));
 }
 
+/////////////////////////
 // Primary Functionality
-
+/////////////////////////
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
+  if (!inputValue) return;
+
   addItemToDB(inputValue);
   clearinput();
-  fetchListFromDB(shoppingListRef);
 });
 
-// Helper UI Functions
+shoppingList.addEventListener("click", async (event) => {
+  const curItemId = event.target.getAttribute("data-id");
+  deleteItemFromList(curItemId);
+});
 
+/////////////////////////
+// Helper UI Functions
+/////////////////////////
 function clearinput() {
   inputFieldEl.value = "";
 }
@@ -63,10 +87,20 @@ function clearList(element) {
   element.innerHTML = "";
 }
 
-function createListItem(itemData) {
-  const li = document.createElement("li");
-  li.textContent = itemData;
-  li.classList.add("shopping-list-item");
+function createListItem(itemData, itemId) {
+  if (itemData && itemId) {
+    const li = document.createElement("li");
+    li.textContent = itemData;
+    li.classList.add("shopping-list-item");
+    li.setAttribute("data-id", itemId);
 
-  shoppingList.appendChild(li);
+    shoppingList.appendChild(li);
+  }
+}
+function showEmptyListElement() {
+  const emptyList = document.createElement("h2");
+  emptyList.textContent =
+    "Looks like you have no items here 🤔 It`s time do add something!";
+  emptyList.classList.add("empty-list");
+  shoppingList.appendChild(emptyList);
 }
